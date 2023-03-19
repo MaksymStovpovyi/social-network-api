b7d94df5847f11f81affd8cdd48308e80275179b
maxSt <stovpovymax@gmail.com>
2023-03-18 22:40:39 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# social-network-api
+# Social Network Api
+
+[Description](#description)---[License](#license)---[Questions](#questions)
+
+## Description
+Using this application.\
+You to be able to view and manage the departments, roles, and employees in your company.\
+[Video demonstration](https://drive.google.com/file/d/1V2MMCCvQP6EBEp8hEo14gf-bB4xa2kJB/view)
+
+## License
+
+The Unlicense [more info](https://choosealicense.com/licenses/)
+
+## Questions
+
+For communication use:\
+git: [MaksymStovpovyi](https://github.com/MaksymStovpovyi/)\
+email: stovpovymax@gmail.com
--- utils/data.js
+++ utils/data.js
@@ -1,13 +1,23 @@
 const usernames = ['Bob', 'Tom', 'Joe'];
 const emails = ['bob@gmail.com','tom@gmail.com','joe@gmail.com'];
 
-const thoughts = [];
-for (let i = 0; i < 3; i++) {
-    thoughts.push({
-        thoughtText: `This is thought ${i + 1}`,
-        username: usernames[Math.floor(Math.random() * usernames.length)],
+const thoughts = [
+    {
+        thoughtText: 'Some thought... I',
+        name: 'Bob',
         createdAt: new Date()
-    });
-};
+    },
+    {
+        thoughtText: 'Some thought... II',
+        name: 'Tom',
+        createdAt: new Date()
+    },
+    {
+        thoughtText: 'Some thought... III',
+        name: 'Joe',
+        createdAt: new Date()
+    }
+];
 
+console.log(thoughts);
 module.exports = { usernames, emails, thoughts};
